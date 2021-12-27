6ce347d14d3cd689ab962a5f8ebdf08f4dba3df4
Sergey Dolgikh <ageres7@gmail.com>
2021-12-28 00:46:27 +0500

    Initial Commit

---
+++ CoreDataProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreDataProject
+//
+//  Created by Sergey Dolgikh on 28.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Sergey Dolgikh on 28.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
